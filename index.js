0a2bbe3fbe8e4745c0c8ae0c6f9516a7009588ef
mfidjel <mahfoud.fidjel@gmail.com>
2022-04-16 09:54:33 +0200

    creating basic express server

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Ici nous allons créer le serveur
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello');
+})
+
+app.get('/health', (req, res) => {
+    res.send('I');
+})
+
+app.listen('8080', () => {
+    console.log("Server running");
+})
